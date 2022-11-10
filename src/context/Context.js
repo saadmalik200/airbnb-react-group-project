e6a11c10cb99c689bcdeb16e1ed63959a4d19be6
Saad Malik <saad_malik200@hotmail.com>
2022-11-11 00:37:57 +0100

    fixed card

--- src/context/Context.js
+++ src/context/Context.js
@@ -25,7 +25,7 @@ const ContextProvider = ({ children }) => {
         return { ...state, distance: [...state.distance, action.payload] };
 
       case "geoData":
-        // console.log("action", action.payload);
+        console.log("This is Geo Data", action.payload);
         return { ...state, geoData: action.payload };
 
       default:
