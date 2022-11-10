4f65c1d4eeccdf9c685e3a92ee7d18a0b89ca08e
Saad Malik <saad_malik200@hotmail.com>
2022-11-10 20:08:53 +0100

    routes created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import LocalData from "./components/localdata/LocalData";
-import CardConatiner from "./components/card/CardConatiner";
 import SingleHouseDetails from "./components/singlehousedetails/SingleHouseDetails";
-import NameReviewHeader from "./components/singlehousedetails/top-name-review/NameReviewHeader";
+import HomePage from "./components/homepage/HomePage";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <LocalData />
-      <NameReviewHeader />
-      <CardConatiner />
-      <SingleHouseDetails />
+      <BrowserRouter>
+        <LocalData />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/search" element={<Header />} />
+          <Route path="/home/:singlehome" element={<SingleHouseDetails />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
