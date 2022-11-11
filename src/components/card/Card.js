e9c8fc2814e571c948287bfb0c462ee6e4c7e1b6
Saad Malik <saad_malik200@hotmail.com>
2022-11-11 21:05:48 +0100

    changed Card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,12 +1,12 @@
 import * as React from "react";
-import AspectRatio from "@mui/joy/AspectRatio";
-import Avatar from "@mui/joy/Avatar";
+
 import Box from "@mui/joy/Box";
 import Card from "@mui/joy/Card";
-import IconButton from "@mui/joy/IconButton";
 import Typography from "@mui/joy/Typography";
-import Link from "@mui/joy/Link";
 import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded";
+
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { BsHeartFill } from "react-icons/bs";
 import Carousel from "react-material-ui-carousel";
 import { Context } from "../../context/Context";
 import { useContext } from "react";
@@ -16,27 +16,19 @@ import { BsFillStarFill } from "react-icons/bs";
 
 export default function ContainerResponsive({ item, i }) {
   const { state, dispatch } = useContext(Context);
-  //console.log("this file", state);
-  // const images = [
-  //   "https://a0.muscache.com/im/pictures/4532d3d4-dd10-472c-bc9b-fda35645f074.jpg?im_w=1200",
-  //   "https://a0.muscache.com/im/pictures/42943aae-a61d-44cf-ac74-032872b406ec.jpg?im_w=720",
-  //   "https://a0.muscache.com/im/pictures/f080ece9-3d5d-4686-b1f3-fbd66ae8f181.jpg?im_w=720",
-  // ];
-
-  // console.log(state);
 
   return (
-    <Box sx={{ height: 418.34 }}>
+    <Box sx={{ height: 399.61, width: 315.39, resize: 0 }}>
       <GeoData lat={item.latlong[0]} lng={item.latlong[1]} />
       <Card
         // variant="outlined"
         sx={(theme) => ({
           padding: 0,
-          width: 335.22,
-          height: 418.34,
+          width: 315.39,
+          height: 399.61,
           gridColumn: "span 2",
-          flexDirection: "row",
-          flexWrap: "wrap",
+          // flexDirection: "row",
+          // flexWrap: "wrap",
           resize: "horizontal",
           overflow: "hidden",
           // gap: "clamp(0px, (100% - 360px + 32px) * 999, 16px)",
@@ -45,7 +37,7 @@ export default function ContainerResponsive({ item, i }) {
             borderColor: theme.vars.palette.primary.outlinedHoverBorder,
             transform: "translateY(-2px)",
           },
-          "& > *": { minWidth: "clamp(0px, (360px - 100%) * 999,100%)" },
+          // "& > *": { minWidth: "clamp(0px, (360px - 100%) * 999,100%)" },
         })}
       >
         <Box
@@ -53,54 +45,48 @@ export default function ContainerResponsive({ item, i }) {
             padding: 0,
             display: "flex",
             flexDirection: "column",
-            width: 335.22,
+            // alignItems: "center",
+            width: 315.39,
+            // height: 318.44,
           }}
         >
-          <div
+          {/* <div
             size="sm"
             variant="plain"
             color="neutral"
             sx={{ ml: "auto", alignSelf: "flex-start" }}
           >
-            {" "}
-            <FavoriteBorderRoundedIcon
-              color="danger"
-              sx={{
-                bottom: "-120%",
-                left: "-100%",
-                position: "relative",
+            {" "} */}
 
-                color: "maroon",
-              }}
-            />
-          </div>
+          {/* </div> */}
           <Carousel
             interval={null}
             sx={{
-              height: 318.44,
+              height: 299.61,
+              position: "relative",
             }}
             indicatorContainerProps={{
               style: {
                 marginTop: "-30px", // 5
                 textAlign: "center", // 4
-                zIndex: "99",
+                zIndex: "1",
               },
             }}
             indicatorIconButtonProps={{
               style: {
                 padding: "2px", // 1
-                height: "1px",
+                height: "2px",
                 marginRight: "10px",
-                width: "1px",
-                color: "white", // 3
+                width: "2px",
+                color: "red", // 3
               },
             }}
           >
             {item?.images.map((image, i) => (
               <div
                 style={{
-                  height: "318.44px",
-                  width: "335.22px",
+                  height: "299.61px",
+                  width: "315.39px",
                   zIndex: "-1",
                 }}
               >
@@ -112,6 +98,7 @@ export default function ContainerResponsive({ item, i }) {
                     height: "100%",
                     borderRadius: "15px",
                     zIndex: "-5",
+                    marginBottom: "12px",
                   }}
                 />
               </div>
@@ -123,14 +110,38 @@ export default function ContainerResponsive({ item, i }) {
           sx={{
             display: "flex",
             gap: 0,
-            mt: "auto",
+            mt: "12px",
             fontSize: "15px",
-            lineHeight: "19px",
+            lineHeight: "21px",
           }}
         >
           <div>
+            <FavoriteBorderIcon
+              className="hover:fill-red-400"
+              style={{
+                // outline: "1px white solid",
+                color: "white",
+                fill: "red",
+                position: "absolute",
+                right: "1.5rem",
+                top: "1.5rem",
+                fontSize: "2rem",
+                zIndex: "999",
+              }}
+            />
+            {/* <FavoriteBorderRoundedIcon
+              // color="danger"
+              sx={{
+                position: "absolute",
+                bottom: "10rem",
+                left: "10rem",
+                zIndex: "100",
+                color: "maroon",
+              }}
+            /> */}
+
             <Typography style={{ display: "flex" }} level="h2" fontWeight="lg">
-              <p className="w-[28rem]">
+              <p className="w-[26rem]">
                 {item.city.slice(0, 15)} {item.country}{" "}
               </p>
               <div className="flex gap-2 items-center">
@@ -144,7 +155,7 @@ export default function ContainerResponsive({ item, i }) {
             <Typography sx={{ color: "gray" }} level="body2">
               {item.date}
             </Typography>
-            <Typography level="body2">
+            <Typography style={{ marginTop: 6 }} level="body2">
               <strong>€ {item.price}</strong> night
             </Typography>
           </div>
