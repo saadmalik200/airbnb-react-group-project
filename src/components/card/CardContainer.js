97ac0da322db4ae15140fcdc288f08aecbf264b5
harshita <harshita1225@gmail.com>
2022-11-12 00:11:12 +0100

    created map

--- src/components/card/CardContainer.js
+++ src/components/card/CardContainer.js
@@ -1,16 +1,21 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../../context/Context";
 import Card from "./Card";
 
 const CardContainer = () => {
   const { state } = useContext(Context);
+  const navigate = useNavigate();
+
   return (
     <div className="flex mt-10  justify-center gap-10 flex-wrap">
       {state?.localData?.map((item, i) => (
-        <Link key={i} to={`/home/${item.id}`}>
+        <div
+          style={{ cursor: "pointer" }}
+          onClick={() => navigate(`/home/${item?.id}`)}
+        >
           <Card item={item} i={i} />
-        </Link>
+        </div>
       ))}
     </div>
   );
--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import CardContainer from "../card/CardContainer";
 import Header from "../header/Header";
+import MyMap from "../map/Map";
 
 const HomePage = () => {
   return (
     <div>
       <Header />
-      <CardContainer />
+      {/*<CardContainer /> */}
+      <MyMap />
     </div>
   );
 };
